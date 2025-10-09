d224359830d65cb4dc598fc8770ff3a547a90560
jonas <jonatorv@stud.ntnu.no>
2025-10-09 15:48:57 +0200

    Update `DiaryEntry` with author field, validation, and info printing
    Added an author attribute to `DiaryEntry`, introduced validation for duration via `setMinDuration`, and implemented `printDiaryInfo` to display entry details. Updated `Main` to demonstrate usage.

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -1,8 +1,17 @@
 package edu.ntnu.iir.bidata;
 
+import edu.ntnu.iir.bidata.model.DiaryEntry;
+
 /**
  * The main starting point of your application. Let this class create the
  * instance of your main-class that starts your application.
  */
 public class Main {
+    public static void main(String[] args) {
+        DiaryEntry diaryEntry = new DiaryEntry("Afternoon run", "Jonas", "4x4. 4 min på, 2 min av", -2, "Running");
+        DiaryEntry diaryEntry2 = new DiaryEntry("Afternoon run", "Jonas", "4x4. 4 min på, 2 min av", -2, "Running");
+        diaryEntry.printDiaryInfo();
+        System.out.println("---------------------------------------");
+        diaryEntry2.printDiaryInfo();
+    }
 }
--- src/main/java/edu/ntnu/iir/bidata/model/DiaryEntry.java
+++ src/main/java/edu/ntnu/iir/bidata/model/DiaryEntry.java
@@ -13,17 +13,40 @@ package edu.ntnu.iir.bidata.model;
 public class DiaryEntry {
   private static int nextId = 0;
   private int id;
+  private String author = null;
   private String title = null;
   private String content = null;
   private int duration;
   private String exerciseType = null;
 
-  DiaryEntry(String title, String content, int duration, String exerciseType) {
+  public DiaryEntry(String title, String author, String content, int duration, String exerciseType) {
     nextId = nextId + 1;
     this.id = nextId;
     this.title = title;
+    setMinDuration(duration);
     this.content = content;
+    this.author = author;
     this.duration = duration;
     this.exerciseType = exerciseType;
   }
+
+  public void setMinDuration(int duration){
+        if (duration <= 0){
+            System.out.println("Duration of the session cannot be less than 0");
+            } else if (duration > 1440){
+            System.out.println("Duration of the session can not exeed 24 hours");
+        } else {
+            this.duration = duration;
+        }
+  }
+
+  public void printDiaryInfo() {
+    System.out.println("ID: " + id);
+    System.out.println("Title: " + title);
+    System.out.println("Author: " + author);
+    System.out.println("Content: " + content);
+    System.out.println("Duration: " + duration + " min");
+    System.out.println("Exercise type: " + exerciseType);
+  }
 }
+
