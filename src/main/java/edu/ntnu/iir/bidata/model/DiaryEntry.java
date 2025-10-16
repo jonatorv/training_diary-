060a884222814a99889bdb8f687c39417e026841
jonas <jonatorv@stud.ntnu.no>
2025-10-16 17:16:41 +0200

    Enhance `DiaryEntry` with setters, additional getters, and improved formatting
    Refactored `DiaryEntry` to include comprehensive setter and getter methods, improved method formatting, and updated documentation for better clarity.

--- src/main/java/edu/ntnu/iir/bidata/model/DiaryEntry.java
+++ src/main/java/edu/ntnu/iir/bidata/model/DiaryEntry.java
@@ -3,12 +3,9 @@ package edu.ntnu.iir.bidata.model;
 /**
  * Diary entry represents a single trainingdiary entry.
  *
- * <p>Each entry stores information about a training session:
- * - A unique ID
- * - Title of the session
- * - Information/content of the session
- * - Duration of the session
- * - Specific exercise (running, cycling, strength)
+ * <p>Each entry stores information about a training session: - A unique ID - Title of the session -
+ * Information/content of the session - Duration of the session - Specific exercise (running,
+ * cycling, strength)
  */
 public class DiaryEntry {
   private static int nextId = 0;
@@ -19,7 +16,14 @@ public class DiaryEntry {
   private int duration;
   private String exerciseType = null;
 
-  public DiaryEntry(String title, String author, String content, int duration, String exerciseType) {
+  /**
+   * The constructor creates a new DiaryEntry object with the given parameters title, author,
+   * content, duration and exerciseType.
+   *
+   * <p>In addition, the constructor also assigns a unique ID to every entry.
+   */
+  public DiaryEntry(
+      String title, String author, String content, int duration, String exerciseType) {
     nextId = nextId + 1;
     this.id = nextId;
     this.title = title;
@@ -29,22 +33,105 @@ public class DiaryEntry {
     this.exerciseType = exerciseType;
   }
 
-  public String getContentOfSession(){
-      return this.content;
-
+    /**
+     *
+     * @return
+     */
+  public int getId()
+  {
+    return this.id;
   }
 
-  public void setMinDuration(int duration){
-        if (duration <= 0){
-            System.out.println("Duration of the session cannot be less than 0");
-            } else if (duration > 1440){
-            System.out.println("Duration of the session can not exeed 24 hours");
-        } else {
-            this.duration = duration;
-        }
+    /**
+     *
+     * @return
+     */
+  public String getTitle()
+  {
+    return this.title;
   }
 
-  public void printDiaryInfo() {
+    /**
+     *
+     * @return
+     */
+  public String getContentOfSession()
+  {
+    return this.content;
+  }
+
+    /**
+     *
+     * @return
+     */
+  public String getAuthor()
+  {
+    return this.author;
+  }
+
+    /**
+     *
+     * @return
+     */
+  public String getExerciseType()
+  {
+    return this.exerciseType;
+  }
+
+    /**
+     *
+     * @param title
+     */
+  public void setNewTitle(String title)
+  {
+      this.title = title;
+  }
+
+    /**
+     *
+     * @param content
+     */
+  public void setNewContent(String content)
+  {
+      this.content = content;
+  }
+
+    /**
+     *
+     * @param author
+     */
+  public void setNewAuthor(String author)
+  {
+      this.author = author;
+  }
+
+    /**
+     *
+     * @param exercise
+     */
+  public void setNewExerciseType(String exercise)
+  {
+      this.exerciseType = exercise;
+  }
+  
+
+    /**
+     *
+     * @param duration
+     */
+  public void setMinDuration(int duration)
+  {
+    if (duration <= 0) {
+      System.out.println("Duration of the session cannot be less than 0");
+    } else if (duration > 1440) {
+      System.out.println("Duration of the session can not exeed 24 hours");
+    } else {
+      this.duration = duration;
+    }
+  }
+
+  public void printDiaryInfo()
+  {
     System.out.println("ID: " + id);
     System.out.println("Title: " + title);
     System.out.println("Author: " + author);
@@ -53,4 +140,3 @@ public class DiaryEntry {
     System.out.println("Exercise type: " + exerciseType);
   }
 }
-
