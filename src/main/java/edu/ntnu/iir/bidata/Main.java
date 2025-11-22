6421cae95acda677119834cf31827fe90cba960c
jonas <jonatorv@stud.ntnu.no>
2025-11-22 12:43:44 +0100

    Improve Javadoc clarity and descriptions in `Main`
    - Enhanced class-level and method-level Javadoc for better readability and structure.
    - Added detailed descriptions for the main method's responsibilities and parameters.

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -3,15 +3,18 @@ package edu.ntnu.iir.bidata;
 import edu.ntnu.iir.bidata.controller.Ui;
 
 /**
- * The main starting point of your application. Let this class create the instance of your
- * main-class that starts your application.
+ * The entry point of the application.
+ *
+ * <p>This class is responsible for creating the Ui instance and starting the training diary application. </p>
  */
 public class Main {
 
   /**
-   * The main method. This is where the application initialization and starting takes place.
+   * The main method of the program
    *
-   * @param args the command line arguments
+   * <p>It initializes the user interface, loads initial data, and starts the application loop.</p>
+   *
+   * @param args the command line arguments (not used)
    */
   public static void main(String[] args) {
     Ui ui = new Ui();
